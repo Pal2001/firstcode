c2e32347156f3ff1f479248e64792147c53a880b
Avinash Pal <91541549+Pal2001@users.noreply.github.com>
2022-09-30 22:20:04 +0530

    practice for function in java

---
+++ function.java
@@ -0,0 +1,146 @@
+public class function_practice {
+
+    // 1) function for printing name 
+
+    // public static void printName( String name){
+    //     System.out.println(name.toUpperCase());
+    //     return;
+    // }
+
+    // 2) function for adding two no. and return sum
+
+    // public static int additionOfTwoNumbers(int a,int b ){
+    //     int sum = a + b;
+    //     return sum;
+    // }
+
+    // 3) function for calculate product
+
+    // public static int calculateProduct(int a ,int b){
+    //     int product = a*b;
+    //     return product;
+    // }
+
+    // 4) function for calculate factorial
+
+    // public static int calfactorial(int a){
+    //     if (a<0){
+    //         System.out.println(" Invalid number ");
+    //         return 0;
+    //     }
+    //     int fact=1;
+    //     for( int i=a; i>=1; i--){
+    //         fact=fact*i;
+    //     }
+    //     return fact;
+    // }
+
+    // 5) function to print  average of numbers
+
+    // public static float Calcaverage(float a , float b , float c){
+    //     float sum = a+b+c;
+    //     float average = sum/3;
+    //     return average;
+    // }
+
+    // 6) function to print sum of odd no. from 1 to n
+
+    // public static int Calcsumodd(int a){
+    //     int sum =0;
+    //     for(int i=a; i>=1; i--){
+    //         if(i%2==0){
+    //            continue; 
+    //         }
+    //         sum = sum + i;    
+    //     }
+    //     return sum;
+         
+    // }
+
+    // 7) input of two no and return gratest one
+
+    // public static void calcuGreatest(int a , int b){
+    //     if(a>b){
+    //         System.out.println(a);
+    //     } else{
+    //         System.out.println(b);
+    //     }
+    // }
+
+    // 8) calculate circumference of circle by taking input of radius
+
+    // public static double calCircumference(int a ){
+    //     double circumference = 2*3.14*a;
+    //     return circumference;
+    // }
+
+    public static void main (String []args){
+        // program for infinite loop for do while
+
+        // int i=1;
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        // } while(i>0);
+
+        // 8) calculate circumference of circle by taking input of radius
+
+        // Scanner sc =new Scanner(System.in);
+        // int n = sc.nextInt();
+        // System.out.println(calCircumference(n));
+
+        // 7) input of two no and return gratest one
+
+        // Scanner sc = new Scanner(System.in);
+        // int x = sc.nextInt();
+        // int y = sc.nextInt();
+        // calcuGreatest(x, y);
+
+        // 6) function to print sum of odd no. from 1 to n
+
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // System.out.println("Sum of all odd no till 'n' is " + Calcsumodd(n));
+
+        // 5) function to print  average of numbers
+
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // System.out.println("Average of three numbers is " + Calcaverage(a, b, c));
+
+        // 4) function for calculate factorial
+
+        // Scanner sc= new Scanner(System.in);
+        // System.out.println("Enter any no. to calculate it's factorial");
+        // int n = sc.nextInt();
+        // System.out.println("factorial of enterd no is " + calfactorial(n));
+
+        // 3) function for calculate product
+
+        // Scanner sc = new Scanner(System.in);
+        // int d = sc.nextInt();
+        // int c = sc.nextInt();
+        // System.out.println("product of two numbers is " + calculateProduct(d, c));
+
+         // 2) function for adding two no. and return sum
+
+        // Scanner sc=new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // System.out.println("sum of two numbers is " +additionOfTwoNumbers(a, b));
+
+        // 1) function for printing name
+
+        // Scanner sc = new Scanner(System.in);
+        // String name =sc.nextLine();
+
+        // printName(name);
+
+       
+
+      
+    }
+    
+}
